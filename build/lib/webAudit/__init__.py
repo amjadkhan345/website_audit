0d11f853cf8b99b5553ab36a357854adbaf22c53
amjadkhan345 <ak4806030@gmail.com>
2025-09-08 23:14:27 +0300

    Initial commit with Readme and project files

--- build/lib/webAudit/__init__.py
+++
@@ -1,4 +0,0 @@
-from .crawler import AuditSpider, check_links
-from .seo import analyze_seo
-from .pagespeed import pagespeed_audit
-from .report import generate_csv_report
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='webAudit',
-    version='0.1.0',
+    version='0.1.1',
     author='Amjad khan',
     author_email='info@shailatech.com',
     description='Full website audit tool (broken links, SEO, PageSpeed)',
